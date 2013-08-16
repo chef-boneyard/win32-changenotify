ba3f7cc1d1a3c205a04ef43a7326566ea1fdba11
Daniel Berger <djberg96@gmail.com>
2013-08-16 10:12:32 -0600

    Added a helper function for nicer windows error messages.

--- lib/win32/changenotify/functions.rb
+++ lib/win32/changenotify/functions.rb
@@ -25,5 +25,27 @@ module Windows
     attach_function :CreateFileA,
       [:string, :dword, :dword, :ptr, :dword, :dword, :handle],
       :handle
+
+    attach_function :FormatMessage, :FormatMessageA,
+      [:ulong, :pointer, :ulong, :ulong, :pointer, :ulong, :pointer], :ulong
+
+
+    # Returns a nicer Windows error message
+    def win_error(function, err=FFI.errno)
+      flags = 0x00001000 | 0x00000200
+      buf = FFI::MemoryPointer.new(:char, 1024)
+
+      FormatMessage(flags, nil, err , 0x0409, buf, 1024, nil)
+
+      function + ': ' + buf.read_string.strip
+    end
+
+    # Shortcut for win_error with raise builtin
+    def raise_windows_error(function, err=FFI.errno)
+      raise SystemCallError.new(win_error(function, err), err)
+    end
+
+    module_function :win_error
+    module_function :raise_windows_error
   end
 end
