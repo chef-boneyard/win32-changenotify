46ef1f57a6e087e9bef344fafc46d524d5ec3ccf
Daniel Berger <djberg96@gmail.com>
2013-08-16 10:12:47 -0600

    Refactored the tests a bit for test-unit 2.x.

--- test/test_win32_changenotify.rb
+++ test/test_win32_changenotify.rb
@@ -4,7 +4,7 @@
 # Test suite for the win32-changenotify package. You should run this
 # test via the 'rake test' task.
 #############################################################################
-require 'test/unit'
+require 'test-unit'
 require 'win32/changenotify'
 include Win32
 
@@ -14,53 +14,71 @@ class TC_Win32_ChangeNotify < Test::Unit::TestCase
     @cn = ChangeNotify.new("c:\\", false, @filter)
   end
 
-  def test_version
+  test "version constant is set to expected value" do
     assert_equal('0.6.0', ChangeNotify::VERSION)
   end
 
-  def test_path
+  test "path basic functionality" do
     assert_respond_to(@cn, :path)
+    assert_nothing_raised{ @cn.path }
+    assert_kind_of(String, @cn.path)
+  end
+
+  test "path returns expected value" do
     assert_equal("c:\\", @cn.path)
   end
 
-  def test_recursive
+  test "recursive basic functionality" do
     assert_respond_to(@cn, :recursive?)
+    assert_nothing_raised{ @cn.recursive? }
+    assert_boolean(@cn.recursive?)
+  end
+
+  test "recursive returns expected value" do
     assert_equal(false, @cn.recursive?)
   end
 
-  def test_filter
+  test "filter method basic functionality" do
     assert_respond_to(@cn, :filter)
+    assert_nothing_raised{ @cn.filter }
+    assert_kind_of(Numeric, @cn.filter)
+  end
+
+  test "filter method returns expected value" do
     assert_equal(@filter, @cn.filter)
   end
 
-  # The errors here are expected because of the timeout.
-  def test_wait
+  test "wait basic functionality" do
     assert_respond_to(@cn, :wait)
-    assert_raises(TypeError){ @cn.wait(0.001) }
-    assert_raises(TypeError){ @cn.wait(0.001){} }
   end
 
-  def test_wait_any
+  test "an error is raised if a timeout occurs" do
+    assert_raise(SystemCallError){ @cn.wait(0.001) }
+    assert_raise(SystemCallError){ @cn.wait(0.001){} }
+  end
+
+  test "inherits wait_any method from Ipc parent" do
     assert_respond_to(@cn, :wait_any)
   end
 
-  def test_wait_all
+  test "inherites wait_all method from Ipc parent" do
     assert_respond_to(@cn, :wait_all)
   end
 
-  def test_expected_errors
-    assert_raises(TypeError){
-      ChangeNotify.new(1, true, @filter)
-    }
-    assert_raises(TypeError){
-      ChangeNotify.new("C:\\", 'foo', @filter)
-    }
-    assert_raises(TypeError){
-      ChangeNotify.new("C:\\", false, "foo")
-    }
-    assert_raises(TypeError){
-      ChangeNotify.new(1, true, @filter, 'bogus')
-    }
+  test "constructor requires the first argument to be a string" do
+    assert_raises(TypeError){ ChangeNotify.new(1, true, @filter) }
+  end
+
+  test "constructor requires second argument to be a boolean" do
+    assert_raises(TypeError){ ChangeNotify.new("C:\\", 'foo', @filter) }
+  end
+
+  test "constructor requires third argument to be numeric" do
+    assert_raises(TypeError){ ChangeNotify.new("C:\\", false, "foo") }
+  end
+
+  test "constructor requires fourth argument to be a Win32::Event object" do
+    assert_raises(TypeError){ ChangeNotify.new(1, true, @filter, 'bogus') }
   end
 
   def teardown
